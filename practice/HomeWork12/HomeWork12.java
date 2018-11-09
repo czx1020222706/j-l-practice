ca3e9fd0b9b8a0bb39e287c2ded87c1c17e2fcfa
陈迹 <chenzhenxin1020@gmail.com>
2018-11-09 23:00:09 +0800

    HomeWork12

---
+++ practice/HomeWork12/HomeWork12.java
@@ -0,0 +1,13 @@
+package HomeWork12;
+public class HomeWork12 {
+    public static void main(String[] args) {
+        int aa=-1;
+        for( int a=0;a<5;a++){
+            aa+=2;
+            for(int b=1;b<=aa;b++){ 
+                System.out.print( "#"  );
+            }
+            System.out.println();}
+         
+    }
+}
